2dadd9e4ecd383fc7ce0443f35b11d1c4e23fe32
Serafim Urukov <serafim.urukov@gmail.com>
2023-09-30 12:30:09 +0300

    Создал репозиторий с неработающим кодом

---
+++ 5. Кафе/main.py
@@ -0,0 +1,60 @@
+import sys
+
+
+def main():
+    """
+    Код программы.
+
+    TODO: Решение неверное. Падает на закрытом тесте #3.
+    """
+    n = int(input())
+    prices = list()
+    for _ in range(n):
+        price = int(input())
+        prices.append(price)
+
+    active_coupons = 0
+    used_coupons = 0
+    total = 0 # Все деньги
+    # NB: индексы, начинаются с 0
+    # todo: отсортировать в конце и +1
+    used_days = []
+    for i in range(len(prices)):
+        # скип дней, в которые использован купон
+        if i in used_days:
+            continue
+
+        # Накопим купон
+        if prices[i] > 100:
+            active_coupons += 1
+
+        # Запишем сумму.
+        total += prices[i]
+        
+        # Можем ли применить купоны?
+        if active_coupons > 0:
+            expensive_val = 0
+            expensive_idx = 0
+            for j in range(i+1, len(prices)):
+                # скип дней, в которые использован купон
+                if j in used_days:
+                    continue
+                if prices[j] > expensive_val:
+                    expensive_val = prices[j]
+                    expensive_idx = j
+            # Применить купон на макс. день
+            # если такой есть.
+            if expensive_idx > 0:
+                active_coupons -= 1
+                used_coupons += 1
+                used_days.append(expensive_idx)
+
+    print(total)
+    print(active_coupons, used_coupons)
+    
+    # отформатировать вывод
+    print(*sorted([i+1 for i in used_days]), sep="\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 5. Кафе/test_main.py
@@ -0,0 +1,19 @@
+import sys, io
+from main import main
+import pytest
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("5\n35\n40\n101\n59\n63\n", "235\n0 1\n5\n"),
+        ("5\n110\n40\n120\n110\n60\n", "260\n0 2\n3\n5\n"),
+        ("3\n110\n110\n110\n", "220\n1 1\n2\n"),
+    ],
+)
+def test_answer(capsys, monkeypatch, test_input, expected):
+    """Проверяет решение. Инпут найден на просторах интернета."""
+    monkeypatch.setattr("sys.stdin", io.StringIO(test_input))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == expected
